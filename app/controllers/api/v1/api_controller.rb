4ee2d27d15277c37e6efe53c78288419ef8190fc
Rodrigo Rosa <rodrigo@wonder.com.br>
2017-11-27 13:34:55 -0200

    Questions Controller

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,7 +3,7 @@ module Api::V1
     rescue_from(ActiveRecord::RecordNotFound) do ||
       render(json: {message: 'Not Found'}, status: :not_found)
     end
- 
+
     rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
        render(json: {message: "Required parameter missing: #{parameter_missing_exception.param}"}, status: :bad_request)
     end
